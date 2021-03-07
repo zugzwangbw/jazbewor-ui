914ffa407b6f9a18ef2c8e95b86bc6c01c974c6c
Elson Júnior <elson.araujo.bw@gmail.com>
2021-03-07 16:17:00 -0400

    fix: ajuste logo principal

--- src/styles/pages/HomeSTL.ts
+++ src/styles/pages/HomeSTL.ts
@@ -30,3 +30,18 @@ export const ImgHomo = styled.section`
       padding-bottom: 200px;
     }
 `
+
+export const ImgJazbeworLogo = styled.section`
+    width: 100%;
+    padding-bottom: calc(100vh - 600px);
+    /* padding: calc(100vh - 30rem); */
+    background: url(./logo-g-full.svg) center center no-repeat;
+    background-size: contain;
+
+    @media screen and (max-width: 768px) {
+      padding-bottom: 200px;
+      /* margin: 0px 50px 0px 50px; */
+      margin-right: 50px;
+      margin-left: 50px;
+    }
+`
