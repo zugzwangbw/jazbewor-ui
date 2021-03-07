e3be64a9e576fbb643430cf8d558d33ffe930721
Elson Júnior <elson.araujo.bw@gmail.com>
2021-03-07 16:43:12 -0400

    fix: ajuste fonte home 02

--- src/styles/pages/HomeSTL.ts
+++ src/styles/pages/HomeSTL.ts
@@ -20,16 +20,17 @@ export const Container = styled.div`
   }
 
     @media screen and (max-width: 768px) {
+        text-align: center;
         h1 {
             font-size: 2rem;
             color: ${props => props.theme.colors.blue};
-            margin-top: 20px;
+            margin-top: 24px;
           }
 
           p {
-            margin-top: 16px;
-            font-size: 16px;
-            line-height: 16px;
+            margin-top: 18px;
+            font-size: 18px;
+            line-height: 24px;
           }
     }
 
@@ -49,14 +50,10 @@ export const ImgHomo = styled.section`
 export const ImgJazbeworLogo = styled.section`
     width: 100%;
     padding-bottom: calc(100vh - 600px);
-    /* padding: calc(100vh - 30rem); */
     background: url(./logo-g-full.svg) center center no-repeat;
     background-size: contain;
 
     @media screen and (max-width: 768px) {
-      padding-bottom: 200px;
-      /* margin: 0px 50px 0px 50px; */
-      margin-right: 50px;
-      margin-left: 50px;
+      padding-bottom: 150px;
     }
 `
